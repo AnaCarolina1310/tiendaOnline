649983cc9d6e5760bd7bdc50e7e92a9969cd317a
AnaCarolina1310 <32752610+AnaCarolina1310@users.noreply.github.com>
2022-01-29 17:29:49 -0300

    varios

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { ItemListContainer } from "./components/ItemListContainer/ItemListContainer";
+import { NavBar } from "./components/NavBar/NavBar";
 import "./styles.css"
 
 
 function App() {
   return (
     <div>
+      <NavBar/>
       <ItemListContainer greeting="Hola Mundo"/>
     </div>
   );
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,12 @@
+export const NavBar = ()=> {
+    return(
+        <header>
+            <h1>Logo</h1>
+            <nav>
+                <p>Enlace 1</p>
+                <p>Enlace 2</p>
+                <p>Enlace 3</p>
+            </nav>
+        </header>
+    )
+}
