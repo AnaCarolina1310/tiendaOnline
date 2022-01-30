6c7608b7d86c6aeae9c7e979f4459a9b9225dcc9
AnaCarolina1310 <32752610+AnaCarolina1310@users.noreply.github.com>
2022-01-30 10:32:02 -0300

    cambios

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,11 +1,11 @@
 export const NavBar = ()=> {
     return(
-        <header>
+        <header className="header">
             <h1>Logo</h1>
-            <nav>
-                <p>Enlace 1</p>
-                <p>Enlace 2</p>
-                <p>Enlace 3</p>
+            <nav className="nav-Bar">
+                <p className="enlaces">Enlace 1</p>
+                <p className="enlaces">Enlace 2</p>
+                <p className="enlaces">Enlace 3</p>
             </nav>
         </header>
     )
