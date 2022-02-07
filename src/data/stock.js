da4a6f159eba803ac4b054c59f8db6319cdc1e59
AnaCarolina1310 <32752610+AnaCarolina1310@users.noreply.github.com>
2022-02-06 21:36:08 -0300

    cambios

--- src/data/stock.js
+++ src/data/stock.js
@@ -1,20 +1,20 @@
 export const stock = [ 
     {
     "code": 1,
-    "nombre": "Body ",
+    "nombre": "Malbec",
     "precio": 2750,
-    "imagen": "https://www.vinetur.com/imagenes/2019/agosto/5/vino_termometro.jpg"
+    "imagen": "https://ep01.epimg.net/elcomidista/imagenes/2020/08/31/articulo/1598909097_396757_1598912731_sumario_normal.jpg"
     },
     {
     "code": 2,
-    "nombre": "Conjunto Bianca",
+    "nombre": "Rosso",
     "precio": 2300,
-    "imagen": "https://www.vinetur.com/imagenes/2019/agosto/5/vino_termometro.jpg"
+    "imagen": "https://www.animalgourmet.com/wp-content/uploads/2020/12/1751-1-1-e1638811445797.jpg"
     },
     {
     "code": 3,
-    "nombre": "Conjunto Giovanna",
+    "nombre": "Bianco",
     "precio": 3500,
-    "imagen": "https://www.vinetur.com/imagenes/2019/agosto/5/vino_termometro.jpg"
+    "imagen": "https://ep01.epimg.net/elcomidista/imagenes/2020/08/31/articulo/1598909097_396757_1598912774_sumario_normal.jpg"
     }
     ]
