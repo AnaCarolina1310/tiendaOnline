7a37484bf86b2fb922070ede41e89f183ae6e376
AnaCarolina1310 <32752610+AnaCarolina1310@users.noreply.github.com>
2022-02-13 11:20:24 -0300

    cambios

--- src/components/ItemDetailContainer/ItemDetailContainer.js
+++ src/components/ItemDetailContainer/ItemDetailContainer.js
@@ -1,11 +1,17 @@
 import React from 'react';
-
+import { useEffect, useState } from "react"
+import { pedirDatos } from "../../helpers/pedirDatos"
 export const ItemDetailContainer = ()=>{
+     const [item, setItem ] = useState(null);
+
+     useEffect(()=>{
+
+     })
 
     return (
         <div>
 
-            
+
         </div>
     )
 }
