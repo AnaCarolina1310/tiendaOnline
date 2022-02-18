a9a5a94656141ff08e695f99169b3301e580f2a4
AnaCarolina1310 <32752610+AnaCarolina1310@users.noreply.github.com>
2022-02-18 09:36:09 -0300

    cambios

--- src/components/ItemCount/ItemCount.js
+++ src/components/ItemCount/ItemCount.js
@@ -3,13 +3,18 @@ import { useState } from "react"
 export const ItemCount = () => {
  
     const [counter, setCounter ] = useState(0)
-
+    const handleSumar = () =>{
+        setCounter(counter + 1)
+    }
+    const handleRestar = () =>{
+        setCounter(counter - 1)
+    }
 
     return (
         <div>
-            <button className="btn btn-outline-primary">-</button>
+            <button className="btn btn-outline-primary" onClick={handleRestar}>-</button>
             <span className="mx-3">{counter}</span>
-            <button className="btn btn-outline-primary">+</button>
+            <button className="btn btn-outline-primary" onClick={handleSumar}>+</button>
         </div>
     )
 }
