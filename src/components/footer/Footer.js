bb74cb50e47ad671b2f1779cf826c4725a52d66b
AnaCarolina1310 <32752610+AnaCarolina1310@users.noreply.github.com>
2022-03-23 13:58:18 -0300

    c

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -2,15 +2,16 @@ import {BsFacebook} from  'react-icons/bs'
 import {BsTwitter} from 'react-icons/bs'
 import {BsInstagram} from 'react-icons/bs'
 import './footer.css'
+
 export  const Footer =()=>{
 
     return(
      <div className="container my-5">
      <hr/>
         <div className='redes'>
-            <BsFacebook className='redesIcons'/>
-            <BsTwitter className='redesIcons'/>
-            <BsInstagram className='redesIcons'/>
+          <a href='https://es-es.facebook.com/'> <BsFacebook className='redesIcons'/></a>
+            <a href='https://twitter.com/?lang=es'><BsTwitter className='redesIcons'/></a>
+           <a href='https://www.instagram.com/?hl=es'><BsInstagram className='redesIcons'/></a>
         </div>
      </div>
     )
