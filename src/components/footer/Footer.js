f6da04f9707f342b93f225d4d160c9ad7f1b268c
AnaCarolina1310 <32752610+AnaCarolina1310@users.noreply.github.com>
2022-03-24 17:22:19 -0300

    c

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,9 +1,13 @@
+
 import {BsFacebook} from  'react-icons/bs'
 import {BsTwitter} from 'react-icons/bs'
 import {BsInstagram} from 'react-icons/bs'
 import './footer.css'
 
+
 export  const Footer =()=>{
+
+ 
 
     return(
      <div className="container my-5">
